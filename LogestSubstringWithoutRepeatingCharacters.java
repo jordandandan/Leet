8deea4a710d4311154ad8a3d35c5457b205a0a8b
jordandandan <jiayixu@tju.edu.cn>
2016-05-08 20:00:43 +0800

    #3 Solution & at most K distinct letters

--- LogestSubstringWithoutRepeatingCharacters.java
+++ LogestSubstringWithoutRepeatingCharacters.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 
 public class LogestSubstringWithoutRepeatingCharacters {
+	/*
+	 * Solution1
+	 */
 	public static int lengthOfLongestSubstring(String s) {
 		boolean[] exist = new boolean[256];
 		int i = 0;
@@ -21,6 +24,9 @@ public class LogestSubstringWithoutRepeatingCharacters {
 		return maxLen;
         
     }
+	/*
+	 * Solution2
+	 */
 	public static int lengthOfLongestSubstring2(String s){
 		int[] index = new int[256];
 		Arrays.fill(index, -1);
@@ -36,12 +42,38 @@ public class LogestSubstringWithoutRepeatingCharacters {
 		}
 		return maxLen;
 	}
+	/*
+	 * The solution for existing k repeated letters at most
+	 */
+	public static int lengthOfLongestKDistinctSubstring(String s,int k){
+		int numberType = 0;
+		int[] map = new int[256];
+		Arrays.fill(map, 0);
+		int maxLen = 0;
+		int i = 0;
+		for(int j=0;j<s.length();j++){
+		
+			if(map[s.charAt(j)] == 0)
+				numberType++;
+			map[s.charAt(j)]++;
+			while(numberType>k){
+				map[s.charAt(i)]--;
+				
+				if(map[s.charAt(i)]==0)
+					numberType--;
+				i++;
+			}
+			maxLen = Math.max(j-i+1, maxLen);
+		}
+		return maxLen;
+	}
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
 		while(s.hasNext()){
 			String in = s.nextLine();
 			System.out.println(lengthOfLongestSubstring(in));
 			System.out.println(lengthOfLongestSubstring2(in));
+			System.out.println(lengthOfLongestKDistinctSubstring(in, 2));
 		}
 	}
 }
