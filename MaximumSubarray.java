402d256183806e89ee51eda752db9ded3c4b4638
jordandandan <jiayixu@tju.edu.cn>
2016-05-21 17:55:32 +0800

    #53 solution

---
+++ MaximumSubarray.java
@@ -0,0 +1,14 @@
+
+public class MaximumSubarray {
+	 	public int maxSubArray(int[] nums) {
+	        int n = nums.length;
+	        int[] dp = new int[n];
+	        int max = nums[0];
+	        dp[0] = nums[0];
+	        for(int i=1;i<n;i++){
+	        	dp[i] = nums[i] + (dp[i-1]>0?dp[i-1]:0);
+	        	max = Math.max(max, dp[i]);
+	        }
+	        return max;
+	    }
+}
