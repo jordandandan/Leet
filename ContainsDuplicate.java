5b45ee65c5dd88c61897d91501ff226d70544608
jordandandan <jiayixu@tju.edu.cn>
2016-05-28 22:17:36 +0800

    #217 solution

---
+++ ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+	public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+        	if(!set.add(nums[i]))
+        		return true;
+        	
+        }
+        return false;
+    }
+}
