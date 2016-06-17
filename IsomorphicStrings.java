e7220b6c830e534a8eedc9331f50c654c08d56b6
jordandandan <jiayixu@tju.edu.cn>
2016-06-17 22:54:05 +0800

    #205 solution

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 
 public class IsomorphicStrings {
+	//using array
 	 public boolean isIsomorphic(String s, String t) {
 		 if(s==null&&t==null)
 			 return true;
@@ -15,4 +18,14 @@ public class IsomorphicStrings {
 		 }
 		 return true;
 	  }
+	 //using map
+	 public boolean isIsomorphic1(String s,String t){
+		 Map<Character,Integer> map1 = new HashMap<Character, Integer>();
+		 Map<Character,Integer> map2 = new HashMap<Character, Integer>();
+		 for(Integer i=0;i<s.length();i++){//we should make a reference compare below,if we use int, then we make two different obj at the //same time
+			 if(map1.put(s.charAt(i), i)!=(map2.put(t.charAt(i), i)))//the put method return the previous value of same key
+				 return false;
+		 }
+		 return true;
+	 }
 }
