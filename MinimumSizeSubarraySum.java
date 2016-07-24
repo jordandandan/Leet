994b0c0136410006f6e5dd4080703920052bba7f
jordandandan <jiayixu@tju.edu.cn>
2016-07-24 21:10:55 +0800

    #209 solution

---
+++ MinimumSizeSubarraySum.java
@@ -0,0 +1,14 @@
+
+public class MinimumSizeSubarraySum {
+	public int minSubArrayLen(int s, int[] nums) {
+        int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
+        while(j<nums.length){
+        	sum += nums[j++];
+        	while(sum>=s){
+        		min = Math.min(min, j-i);
+        		sum-=nums[i++];
+        	}
+        }
+        return min==Integer.MAX_VALUE?0:min;
+    }
+}
